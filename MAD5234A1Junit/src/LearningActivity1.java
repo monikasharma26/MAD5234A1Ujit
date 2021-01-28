dd709ad023a2bd84539b25f648080e8e50c538fb
monikasharma26 <monikasharma26081991@gmail.com>
2021-01-28 15:07:13 -0500

    [monika] First commi t

---
+++ MAD5234A1Junit/src/LearningActivity1.java
@@ -0,0 +1,20 @@
+
+public class LearningActivity1 {
+	
+	public static void main(String[] args) 
+	{
+		Calculator c = new Calculator();
+	//	System.out.println("Hello");
+	}
+
+}
+
+class Calculator{
+	
+	private float result;
+	
+	public float add(float a, float b) {
+		return a+b;
+		
+	}
+}
