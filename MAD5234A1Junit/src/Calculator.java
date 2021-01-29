5e1139bf9fd79e58250e25521c84c140ebbedd02
monikasharma26 <60120985+monikasharma26@users.noreply.github.com>
2021-01-28 20:15:58 -0500

    Update and rename LearningActivity1.java to Calculator.java

---
+++ MAD5234A1Junit/src/Calculator.java
@@ -0,0 +1,22 @@
+class Calculator{
+	//Change class name
+
+	public float add(float a, float b) {
+		return a+b;
+		
+	}
+	
+	public float subtraction(float a, float b) {
+		return a-b;
+		
+	}
+	
+	public float multiply(float a, float b) {
+		return a*b;
+		
+	}
+	public float divide(float a, float b) {
+		return a/b;
+		
+	}
+}
